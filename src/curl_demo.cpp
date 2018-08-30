583c102b700314c98df927b2e368fc8f369bc64e
miki0w0 <miki0w0@hotmail.com>
2018-08-30 10:04:33 +0800

    curl post

--- src/curl_demo.cpp
+++ src/curl_demo.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <iostream> 
 #include <string>
+#include <fstream>
 #include <sstream>
 #include <json/json.h>
 
@@ -20,64 +21,195 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
     return size * nmemb;
 }
 
+int http_post_file(const char *url, const char *filename){
+    CURL *curl = NULL;
+    CURLcode res;
+
+    struct curl_httppost *post=NULL;
+    struct curl_httppost *last=NULL;
+    curl_slist *headers=NULL;
+    bool out = false;
+    if(filename == NULL || url == NULL){
+        printf("<-----------no url or filename----------->\n");
+        return -1;
+    }
+
+    printf("URL: %s\n", url);
+    printf("filename: %s\n", filename);
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    /* Add simple file section */
+    if( curl_formadd(&post, &last, CURLFORM_COPYNAME, "result",
+               CURLFORM_FILECONTENT, filename, 
+               CURLFORM_CONTENTHEADER, headers,
+               CURLFORM_END) != 0)
+    {
+        fprintf(stderr, "curl_formadd error.\n");
+        out = true;
+    }
+    if(out == false){
+    //curl_global_init(CURL_GLOBAL_ALL);
+        curl = curl_easy_init();
+        if(curl == NULL)
+        {
+            fprintf(stderr, "curl_easy_init() error.\n");
+            out = true;
+        }
+    }
+
+    if(out == false){
+        // curl_easy_setopt(curl, CURLOPT_HEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_URL, url); /*Set URL*/
+        curl_easy_setopt(curl, CURLOPT_HTTPPOST, post);
+        int timeout = 5;
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
+        // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+        // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1);
+
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK)
+        {
+            fprintf(stderr, "curl_easy_perform[%d] error.\n", res);
+            out = true;
+        }
+    }
+
+    if(out == false){
+        curl_easy_cleanup(curl);
+    }
+    curl_formfree(post);
+    curl_slist_free_all(headers);
+    //curl_global_cleanup();
+    return 0;
+}
+
+int http_post_json(const char *url, const Json::Value json_value){
+    CURL *curl = NULL;
+    CURLcode res;
+
+    Json::StreamWriterBuilder wbuilder;
+    wbuilder["indentation"] = "";
+    std::string post_str = Json::writeString(wbuilder, json_value);
+    // std::string post_str = json_value.toStyledString();
+
+    struct curl_httppost *post=NULL;
+    struct curl_httppost *last=NULL;
+    struct curl_slist *headers=NULL;
+    headers = curl_slist_append(headers, "Content-Type:application/json;charset=UTF-8");
+
+    if(json_value == NULL || url == NULL){
+        printf("<-----------no url or filename----------->\n");
+        return -1;
+    }
+
+    printf("URL: %s\n", url);
+    printf("post_str: %s\n", post_str.data());
+    
+    /* Add simple file section */
+    curl = curl_easy_init();
+    if(curl == NULL)
+    {
+        fprintf(stderr, "curl_easy_init() error.\n");
+    }
+
+    curl_easy_setopt(curl, CURLOPT_POST, 1);//设置为非0表示本次操作为POST
+    curl_easy_setopt(curl, CURLOPT_HEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_URL, url); /*Set URL*/
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_str.c_str());
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, post_str.size());
+    int timeout = 5;
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
+    // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+    // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1);
+
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform[%d] error.\n", res);
+    }
+    curl_easy_cleanup(curl);
+    curl_formfree(post);
+    curl_slist_free_all(headers);
+    //curl_global_cleanup();
+    return 0;
+}
+
+int check_file(char *filename)
+{
+    ifstream fd;
+    fd.open(filename,std::ios::in);
+    // fd = open(filename, O_RDONLY);
+    if(!fd)
+    {
+        perror("can not open file");
+        return -1;
+    }
+    fd.seekg(0,ios::end);
+    int file_len = fd.tellg();
+    printf("file length: %d",file_len);
+    fd.close();
+    return file_len;
+}
+
 //POST json
 int main()
 {
-    CURL *curl;
-    CURLcode res;
-    char tmp_str[256] = { 0 };
-    std::stringstream out;
 
-    //HTTP报文头  
-    struct curl_slist* headers = NULL;
+    char *url = "http://127.0.0.1:7999/api/result";
+    char *filename = "/home/miki0w0/gree/results.json";
+    
+    //构建json
+    Json::Value root;
+    root["n1"]=Json::Value(1);
+    root["n2"]=Json::Value(2);
+    http_post_json(url, root);
 
-    char *url = "http://127.0.0.1:7999/api/fun1";
+    
+    // if(check_file(filename) == -1){
+    //     printf("file cannot open");
+    //     return 1;
+    // }
+    // curl = curl_easy_init();
 
-    curl = curl_easy_init();
-
-    if(curl)
-    {
-        //构建json
-        Json::Value item;
-        item["n1"]=Json::Value(1);
-        item["n2"]=Json::Value(2);
+    // if(curl)
+    // {
+        
         // item["type"]=Json::Value("Libcurl HTTP POST Json串");
         // item["authList"]=Json::Value("weidong0925@126.com");
-        std::string jsonout = item.toStyledString();
+        // std::string jsonout = item.toStyledString();
 
-        // jsonout = AsciiToUtf8(jsonout);
+    //     // jsonout = AsciiToUtf8(jsonout);
 
-        //设置url
-        curl_easy_setopt(curl, CURLOPT_URL, url);
+    //     //设置url
+    //     curl_easy_setopt(curl, CURLOPT_URL, url);
 
-        //设置http发送的内容类型为JSON
-        //构建HTTP报文头  
-        // sprintf_s(tmp_str, "Content-Length: %s", jsonout.c_str());
-        snprintf(tmp_str,sizeof(tmp_str),"Content-Length: %s", jsonout.c_str());
-        headers=curl_slist_append(headers, "Content-Type:application/json;charset=UTF-8");
-        //headers=curl_slist_append(headers, tmp_str);//在请求头中设置长度,请求会失败,还没找到原因
+    //     //设置http发送的内容类型为JSON
+    //     //构建HTTP报文头  
+    //     // sprintf_s(tmp_str, "Content-Length: %s", jsonout.c_str());
+        // snprintf(tmp_str,sizeof(tmp_str),"Content-Length: %s", jsonout.c_str());
+        // headers=curl_slist_append(headers, "Content-Type:application/json;charset=UTF-8");
+    //     //headers=curl_slist_append(headers, tmp_str);//在请求头中设置长度,请求会失败,还没找到原因
 
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-        //curl_easy_setopt(curl,  CURLOPT_CUSTOMREQUEST, "POST");//自定义请求方式
-        curl_easy_setopt(curl, CURLOPT_POST, 1);//设置为非0表示本次操作为POST
+    //     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    //     //curl_easy_setopt(curl,  CURLOPT_CUSTOMREQUEST, "POST");//自定义请求方式
+    //     curl_easy_setopt(curl, CURLOPT_POST, 1);//设置为非0表示本次操作为POST
 
-        // 设置要POST的JSON数据
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonout.c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, jsonout.size());//设置上传json串长度,这个设置可以忽略
+    //     // 设置要POST的JSON数据
+    //     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonout.c_str());
+    //     curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, jsonout.size());//设置上传json串长度,这个设置可以忽略
 
-        // 设置接收数据的处理函数和存放变量
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);//设置回调函数
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);//设置写数据
-        res = curl_easy_perform(curl);//执行
+    //     // 设置接收数据的处理函数和存放变量
+    //     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);//设置回调函数
+    //     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);//设置写数据
+    //     res = curl_easy_perform(curl);//执行
 
-        curl_slist_free_all(headers); /* free the list again */
+    //     curl_slist_free_all(headers); /* free the list again */
 
-        string str_json = out.str();//返回请求值 
-        printf("%s",str_json.c_str()); 
+    //     string str_json = out.str();//返回请求值 
+    //     printf("%s",str_json.c_str()); 
 
-        /* always cleanup */ 
-        curl_easy_cleanup(curl);
-    } 
+    //     /* always cleanup */ 
+    //     curl_easy_cleanup(curl);
+    // } 
 
     return 0;
  }
